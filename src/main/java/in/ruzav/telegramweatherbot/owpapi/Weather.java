bf209a8415e937492a63092f875c7a87ffb3211d
mirout <misharuz25@gmail.com>
2021-02-10 17:40:14 +0300

    Weather status add

--- src/main/java/in/ruzav/telegramweatherbot/owpapi/Weather.java
+++ src/main/java/in/ruzav/telegramweatherbot/owpapi/Weather.java
@@ -6,16 +6,17 @@ public class Weather {
     private final double temperature;
     private final double windSpeed;
     private final double windDeg;
+    private final String status;
 
     public Weather(JSONObject json) {
         temperature = json.getJSONObject("main").getDouble("temp");
         windSpeed = json.getJSONObject("wind").getDouble("speed");
-
         windDeg = json.getJSONObject("wind").getDouble("deg");
+        status = json.getJSONArray("weather").getJSONObject(0).getString("main");
     }
 
     @Override
     public String toString() {
-        return String.format("Temperature: %g C; %nWind speed: %f; %nWind degree: %f;", temperature, windSpeed, windDeg);
+        return String.format("Temperature: %g C; %nMain: %s; %nWind speed: %f; %nWind degree: %f;", temperature, status, windSpeed, windDeg);
     }
 }
